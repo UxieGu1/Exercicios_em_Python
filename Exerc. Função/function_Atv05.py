bd1f04446047a318effccebe423422d796105b6b
Uxie_Gui <111542995+UxieGu1@users.noreply.github.com>
2024-05-10 19:16:07 -0300

    Update function_Atv05.py

--- Exerc. Função/function_Atv05.py
+++ Exerc. Função/function_Atv05.py
@@ -1,2 +1,10 @@
 """Faça uma função que retorne o reverso de um número inteiro informado. No caso o número
 será uma String Por exemplo: 127 -> 721"""
+
+def reverse(valor):
+    size = len(valor) - 1 
+    for c in range(size, -1, -1):
+        print(valor[c])
+
+string = input(f'Digite o valor/texto a ser revertido: ')
+reverse(string)
