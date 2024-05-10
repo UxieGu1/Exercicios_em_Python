906b52031c4ca23f94a063c7015f4eaaea21d831
Uxie_Gui <111542995+UxieGu1@users.noreply.github.com>
2024-05-10 19:20:20 -0300

    Update function_Atv06.py

--- Exerc. Função/function_Atv06.py
+++ Exerc. Função/function_Atv06.py
@@ -1,2 +1,14 @@
 """Crie uma função que receba como entrada uma string e retorne “É palíndromo” se a string for
 um palíndromo e “Não é palíndromo” caso contrário"""
+
+def palindromo(string):
+    string = string.replace(" ", "").lower()
+    
+    if string == string[::-1]:
+        return "É palíndromo"
+    else:
+        return "Não é palíndromo"
+
+
+print(palindromo("Ame a ema"))  
+print(palindromo("Python"))    
