7dcb44f9a99a91fa977f4ed8ac507c56da2e0b3b
Uxie_Gui <111542995+UxieGu1@users.noreply.github.com>
2024-05-10 18:50:27 -0300

    Update function_Atv03.py

--- Exerc. Função/function_Atv03.py
+++ Exerc. Função/function_Atv03.py
@@ -2,3 +2,9 @@
 formais: taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem e
 custo, que é o custo de um item antes do imposto. A função “altera” o valor de custo para incluir o
 imposto sobre venda"""
+
+def somaImposto(taxaImposto, custo ):
+    resultado_imoposto = custo + (custo * taxaImposto / 100)
+    return resultado_imoposto
+
+somaImposto(20, 120.00)
