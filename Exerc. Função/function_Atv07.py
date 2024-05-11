48a531e2e0e7fc8117ec14e91c630d965d7081cf
Uxie_Gui <111542995+UxieGu1@users.noreply.github.com>
2024-05-11 09:44:24 -0300

    Update function_Atv07.py

--- Exerc. Função/function_Atv07.py
+++ Exerc. Função/function_Atv07.py
@@ -5,3 +5,24 @@ esse será o desconto que o cliente ganhará. Em seguida faça outra função qu
 de desconto e o preço da compra e em seguida imprima o valor descontado e o novo preço
 que será pago
 """
+def contarVogal(string):
+    vogal_a = 'a'       
+    cont = 0
+    for letra in string: 
+        if letra.lower() == vogal_a:
+            cont += 1
+    return cont
+    print(f'O total de letras "a" é {cont}')
+
+def desconto(valorOriginal, percDesconto):
+    valorDesconto = valorOriginal * (percDesconto / 100)
+    valorFinal = valorOriginal - valorDesconto
+    print(f'O desconto será de {valorOriginal - valorFinal}' )
+    return valorFinal
+
+frase = input('Digite sua frase: ').lower()
+resultado = contarVogal(frase)
+print(f'O total de letras "a" na frase é {resultado}')
+
+valor = int(input('Digite o valor original da compra: '))
+desconto(valor, resultado)
