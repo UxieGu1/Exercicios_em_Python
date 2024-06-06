c66f309d776f750151f1c89e627612d101992a54
Guilherme <111542995+UxieGu1@users.noreply.github.com>
2024-06-06 16:13:33 -0300

    Update ex004.py

--- Exerc. Listas/ex004.py
+++ Exerc. Listas/ex004.py
@@ -1,1 +1,3 @@
-
+#04.Crie um programa que solicite o usuário um número N ímpar maior que 1.
+#Em seguida, preencha uma lista com N números inteiros positivos (suponha que o 
+#usuário sempre digitará números inteiros positivos). Selecione o elemento que está no centro da lista. Ao final, calcule e escreva o fatorial do elemento selecionado
